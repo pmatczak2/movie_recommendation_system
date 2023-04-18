c6874902caf5a1090b293d46605486f54f66e062
peteykienzzz <pmatczak2@gmail.com>
2023-04-18 07:08:28 -0700

    created function get_movie_recommendations and find_similar_movies

--- src/movie_recommendation_system/main.py
+++ src/movie_recommendation_system/main.py
@@ -5,23 +5,46 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
 movies = pd.read_csv("movies.csv")
-
+ratings = pd.read_csv("ratings.csv")
 
 def clean_title(title):
     return re.sub("[^a-zA-Z0-9 ]", "", title)
 
-def search(title):
+def get_movie_recommendations(title):
     title = clean_title(title)
     query_vec = vectorizer.transform([title])
     similarity = cosine_similarity(query_vec, tfidf).flatten()
-    indeices = np.argpartition(similarity, -5)[-5:]
-    result = movies.iloc[indeices][::-1]
+    indices = np.argpartition(similarity, -5)[-5:]
+    result = movies.iloc[indices][::-1]
     return result
 
+def find_similar_movies(movie_id):
+    similar_users = ratings[(ratings["movieId"] == movie_id) & (ratings["rating"] > 4)]["userId"].unique()
+    similar_users_recs = ratings[(ratings["userId"].isin(similar_users)) & (ratings["rating"] > 4)]["movieId"]
 
+    similar_users_recs = similar_users_recs.value_counts() / len(similar_users)
+    similar_users_recs = similar_users_recs[similar_users_recs > .10]
+
+    all_users = ratings[(ratings["movieId"].isin(similar_users_recs.index)) & (ratings["rating"] > 4)]
+    all_users_recs = all_users["movieId"].value_counts() / len(all_users["userId"].unique())
+
+    rec_percentages = pd.concat([similar_users_recs, all_users_recs], axis=1)
+    rec_percentages.columns = ["similar", "all"]
+
+    rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
+
+    rec_percentages = rec_percentages.sort_values("score", ascending=False)
+    return rec_percentages.head(10).merge(movies, left_index=True, right_on="movieId")[["score", "title", "genres"]]
+
+# Preprocess movie titles for cosine similarity
 movies["clean_title"] = movies["title"].apply(clean_title)
 
+# Create TF-IDF vectorizer and fit on movie titles
 vectorizer = TfidfVectorizer(ngram_range=(1,2))
-
 tfidf = vectorizer.fit_transform(movies["clean_title"])
 
+# Example usage of get_movie_recommendations() function
+movie_title = input("Enter a movie title: ")
+recommendations = get_movie_recommendations(movie_title)
+print("Movie Recommendations:")
+print(recommendations[["title", "genres"]])
